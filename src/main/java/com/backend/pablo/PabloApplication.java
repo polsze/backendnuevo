7b8ece0d1196f34b4bb35af50985241368173bdf
polsze <polbarrios835237@gmail.com>
2023-02-28 20:21:32 -0300

    fixing docker 10

--- src/main/java/com/backend/pablo/PabloApplication.java
+++ src/main/java/com/backend/pablo/PabloApplication.java
@@ -2,8 +2,11 @@ package com.backend.pablo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
 
 @SpringBootApplication
+@Component
 public class PabloApplication {
 
 	public static void main(String[] args) {
